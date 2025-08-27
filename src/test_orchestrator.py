3daafc499ebea9758c9f0f3ad66db94b6ac39235
Kai <kai902000@gmail.com>
2025-08-27 12:05:43 +0800

    Update test_orchestrator.py

--- src/test_orchestrator.py
+++ src/test_orchestrator.py
@@ -70,8 +70,8 @@ class TestOrchestrator:
                 'duration_seconds': 180,
                 'messages_per_second': 50000,
                 'message_size_bytes': 4096,
-                'num_producer_threads': 4,
-                'num_consumers': 4
+                'num_producer_threads': 12,
+                'num_consumers': 6
             }
         }
     
